fb14a6d176983c7dccc6e879c4f1c76a574ab41c
jstloyal <jstloyalty@gmail.com>
2020-09-14 06:42:53 +0100

    Add about page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import {navbar} from './js/navigation';
-import {home} from './js/home';
-import {footer} from './js/footer';
+import { navbar } from './js/navigation';
+import { home } from './js/home';
+import { about } from './js/about';
+import { contact } from './js/contact';
+import { footer } from './js/footer';
 // console.log("Hello World")
 
 const content = document.querySelector('#content');
 content.appendChild(navbar);
 content.appendChild(home);
+content.appendChild(about);
+content.appendChild(contact);
 content.appendChild(footer);
---
+++ src/js/contact.js
@@ -0,0 +1,9 @@
+export const contact = (() => {
+  const content = document.createElement('div');
+  content.setAttribute('id', 'contactContent');
+  
+  const contactInfo = document.createElement('div');
+  contactInfo.setAttribute('id', 'contactInfo');
+  const contactH2 = document.createElement('h2');
+  contactH2.textContent = 'CONTACT';
+})
